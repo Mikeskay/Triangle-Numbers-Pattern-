c98133c5e9c6a1ececbad5e553fb65daceec2f42
Mikeskay <47972946+Mikeskay@users.noreply.github.com>
2022-03-21 15:56:29 +0000

    Create Pattern.java

---
+++ base/Pattern.java
@@ -0,0 +1,24 @@
+package base;
+
+public class Pattern{
+
+    public static void printPattern (int num){
+
+        for (int i = 1; i <= num; i++ ){
+            for (int j = 1; j <= i; j++ ) {
+
+                System.out.print(j);
+            }
+
+            System.out.println();
+        }
+
+    }
+        public static void main(String[] args) {
+            int num = Integer.parseInt(args[0]);
+            printPattern(num);
+
+
+        }
+
+}
